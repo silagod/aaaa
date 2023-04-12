8d8b80a154e16484922dd294c135bc0d532423a1
Admin <Admin@DESKTOP-9VS7AV9>
2023-04-12 15:33:18 +0800

    oianaw

--- src/whatever/helloworld.java
+++ src/whatever/helloworld.java
@@ -5,10 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 System.out.print("hello world");
 //htg6
-//fawlejf
-//fjeiw
-//bathishigt bichiw
-//dahiad ym bichi	
+//fawefj
 	}
 
 }
